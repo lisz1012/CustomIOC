13187293770920a0792f641653afdb6c4c92b630
lisz1012 <lisz1012@163.com>
2020-07-19 03:25:44 -0700

    StudentService中多个不同类型的Dao，加注释

--- src/main/java/service/StudentService.java
+++ src/main/java/service/StudentService.java
@@ -1,13 +1,19 @@
 package service;
 
 import annotation.Inject;
+import dao.PersonDao;
+import dao.PersonDaoImpl;
 import dao.StudentDao;
 
 public class StudentService {
 	@Inject
 	private StudentDao studentDao;
 
+	@Inject
+	private PersonDao personDao; // 用子类PersonDaoImpl类型也不会出错 ^_^
+
 	public void save() {
+		personDao.save();
 		studentDao.save();
 	}
 }
